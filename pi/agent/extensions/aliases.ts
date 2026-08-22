7c1a3608dff34f12931da2407597524d6e8c6bad
Ramza <ramza@ramza.cc>
2026-08-22 03:33:18 -0500

    pi setup

--- pi/agent/extensions/aliases.ts
+++ pi/agent/extensions/aliases.ts
@@ -2,9 +2,16 @@ import type { ExtensionAPI } from "@earendil-works/pi-coding-agent";
 
 export default function (pi: ExtensionAPI) {
   pi.registerCommand("clear", {
-    description: "Start a new session",
+    description: "Alias for /new",
     handler: async (_args, ctx) => {
       await ctx.newSession();
+    },
+  });
+
+  pi.registerCommand("exit", {
+    description: "Alias for /quit",
+    handler: async (_args, ctx) => {
+      ctx.shutdown();
     },
   });
 }
